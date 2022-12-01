13ec80d9ffd9359b553125f037f1c204b9a83b7f
GET Academy <terje@getacademy.no>
2022-12-01 13:52:35 +0100

    Update Program.cs

--- DbIntro/Program.cs
+++ DbIntro/Program.cs
@@ -17,6 +17,23 @@ using DbIntro;
 
 var connStr = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Contacts;Integrated Security=True";
 var conn = new SqlConnection(connStr);
+
+// INSERT INTO 
+var chris = new Person("Chris", "chris@email.com");
+var insertIntoSql = "INSERT INTO Contacts (Id, FirstName, Email) VALUES (@Id, @FirstName, @Email)";
+var rowsAffected = conn.Execute(insertIntoSql, chris);
+
+// UPDATE 
+chris.Email = "chris@getacademy.no";
+var updateSql = "UPDATE Contacts SET Email = @Email, FirstName = @FirstName WHERE Id = @Id";
+var rowsAffected2 = conn.Execute(updateSql, chris);
+
+// DELETE 
+var obj = new {Id = new Guid("F005FC43-8A45-44F9-B70C-822495A72249")};
+var deleteSql = "DELETE FROM Contacts WHERE Id = @Id";
+var rowsAffected3 = conn.Execute(deleteSql, obj);
+
+
 var sql = "SELECT * FROM Contacts";
 var sql2 = @"SELECT con.Id, con.FirstName, con.Email, com.Name
 FROM Contacts con LEFT JOIN Company com ON con.Company = com.Id";
